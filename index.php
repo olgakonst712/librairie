f7d8b9fe132e5988e109a84e1a9301a708e1be86
olgakonst712 <olgakonstantinova712@gmail.com>
2017-10-17 15:49:57 +0200

    rajout nouveaux dossiers

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php 
+
+
+
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="fr"> 
+<head>
+	<meta charset="utf-8">
+	<meta content="musique" name="keywords">
+	<meta content="Download movies" name="description">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" type="text/css" href="accets/css/jquery.multiscroll.css" />
+	<link rel="stylesheet" href=".public/accet/css/style.css">
+	
+	<title>Elders</title>
+	
+	
+</head>
+<body>
+<div id="myContainer">
+	<div class="ms-left">
+		<div class="ms-section section1">
+		</div>
+        <div class="ms-section section2">
+		</div>
+		<div class="ms-section section3">
+		</div>
+		<div class="ms-section section4">
+	    </div>
+	</div>
+	<div class="ms-right">
+		<div class="ms-section section1">
+		</div>
+		<div class="ms-section section2">
+		</div>
+        <div class="ms-section section3">
+        </div>
+        <div class="ms-section section4">
+        </div>
+		<div class="ms-section section5">
+        </div>
+		<div class="ms-section section6">
+        </div>
+		<div class="ms-section section7">
+        </div>
+		<div class="ms-section section8">
+        </div>
+		<div class="ms-section section9">
+		</div>
+        </div>
+	</div>	
+</div>
+
+
+
+
+</body>
+</html>
