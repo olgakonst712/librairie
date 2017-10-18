71d72436e2204c8a735b7046e7cf2a6edb070589
olgakonst712 <olgakonstantinova712@gmail.com>
2017-10-18 11:57:43 +0200

    chemain

--- index.php
+++ index.php
@@ -12,47 +12,137 @@
 	<meta content="musique" name="keywords">
 	<meta content="Download movies" name="description">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="stylesheet" type="text/css" href="accets/css/jquery.multiscroll.css" />
-	<link rel="stylesheet" href=".public/accet/css/style.css">
+	
+
+	<link rel="stylesheet" href="./public/accet/css/style.css">
 	
 	<title>Elders</title>
 	
 	
 </head>
 <body>
-<div id="myContainer">
-	<div class="ms-left">
-		<div class="ms-section section1">
-		</div>
-        <div class="ms-section section2">
-		</div>
-		<div class="ms-section section3">
-		</div>
-		<div class="ms-section section4">
-	    </div>
-	</div>
-	<div class="ms-right">
-		<div class="ms-section section1">
-		</div>
-		<div class="ms-section section2">
-		</div>
-        <div class="ms-section section3">
-        </div>
-        <div class="ms-section section4">
-        </div>
-		<div class="ms-section section5">
-        </div>
-		<div class="ms-section section6">
-        </div>
-		<div class="ms-section section7">
-        </div>
-		<div class="ms-section section8">
-        </div>
-		<div class="ms-section section9">
-		</div>
-        </div>
-	</div>	
-</div>
+<main class="main-content">
+				<div class="container">
+					<div class="page">
+						<div class="row">
+							<div class="col-md-9">
+								<div class="slider">
+									<ul class="slides">
+										<li><a href="#"><img src="dummy/slide-1.jpg" alt="Slide 1"></a></li>
+										<li><a href="#"><img src="dummy/slide-2.jpg" alt="Slide 2"></a></li>
+										<li><a href="#"><img src="dummy/slide-3.jpg" alt="Slide 3"></a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="row">
+									<div class="col-sm-6 col-md-12">
+										<div class="latest-movie">
+											<a href="#"><img src="dummy/thumb-1.jpg" alt="Movie 1"></a>
+										</div>
+									</div>
+									<div class="col-sm-6 col-md-12">
+										<div class="latest-movie">
+											<a href="#"><img src="dummy/thumb-2.jpg" alt="Movie 2"></a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div> <!-- .row -->
+						<div class="row">
+							<div class="col-sm-6 col-md-3">
+								<div class="latest-movie">
+									<a href="#"><img src="dummy/thumb-3.jpg" alt="Movie 3"></a>
+								</div>
+							</div>
+							<div class="col-sm-6 col-md-3">
+								<div class="latest-movie">
+									<a href="#"><img src="dummy/thumb-4.jpg" alt="Movie 4"></a>
+								</div>
+							</div>
+							<div class="col-sm-6 col-md-3">
+								<div class="latest-movie">
+									<a href="#"><img src="dummy/thumb-5.jpg" alt="Movie 5"></a>
+								</div>
+							</div>
+							<div class="col-sm-6 col-md-3">
+								<div class="latest-movie">
+									<a href="#"><img src="dummy/thumb-6.jpg" alt="Movie 6"></a>
+								</div>
+							</div>
+						</div> <!-- .row -->
+						
+						<div class="row">
+							<div class="col-md-4">
+								<h2 class="section-title">December premiere</h2>
+								<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<ul class="movie-schedule">
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+								</ul> <!-- .movie-schedule -->
+							</div>
+							<div class="col-md-4">
+								<h2 class="section-title">November premiere</h2>
+								<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<ul class="movie-schedule">
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+								</ul> <!-- .movie-schedule -->
+							</div>
+							<div class="col-md-4">
+								<h2 class="section-title">October premiere</h2>
+								<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<ul class="movie-schedule">
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+									<li>
+										<div class="date">16/12</div>
+										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
+									</li>
+								</ul> <!-- .movie-schedule -->
+							</div>
+						</div>
+					</div>
+				</div> <!-- .container -->
+			</main>
 
 
 
