4ef2e4e598c47c6b4bd03750d5d016e76178f17d
olgakonst712 <olgakonstantinova712@gmail.com>
2017-10-23 13:33:28 +0200

    image

--- apps/views/parts/side.php
+++ apps/views/parts/side.php
@@ -25,17 +25,27 @@ echo $users[0]['name']. ' ' . $act[0] . ' a ' . $users[1]['name'];
 echo $users[array_rand($usres)] ['name'] . ' ' . $acte[array_rand($acte)] . ' a ' . $users[array_rand($usres)] ['name']; 
 
 
-function getRandomUser () {
+//function getRandomUser () {
 	debug ($users);
         
       }
+      $used_user = array ();
+
 
       function getRandomUser ($users) {
 	//debug ($users);
 
-      	debug ($users);
-        
+      	//debug ($users);
+      	global $used_user;
+      	if 
       }
+
+
+
+      //getRandomUser ($users) 
+      echo $users[array_rand($usres)] ['name'] . ' ' . $acte[array_rand($acte)] . ' a ' . $users[array_rand($usres)] ['name']; 
+
+
 
 
 ?>
--- index.php
+++ index.php
@@ -14,138 +14,350 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	
 
-	<link rel="stylesheet" href="/public/accet/css/style.css">
+	<link rel="stylesheet" type="text/css" href="public/asset/css/style.css">
 	
 	<title>Elders</title>
 	
 	
 </head>
+
 <body>
-<main class="main-content">
-				<div class="container">
-					<div class="page">
-						<div class="row">
-							<div class="col-md-9">
-								<div class="slider">
-									<ul class="slides">
-										<li><a href="#"><img src="dummy/slide-1.jpg" alt="Slide 1"></a></li>
-										<li><a href="#"><img src="dummy/slide-2.jpg" alt="Slide 2"></a></li>
-										<li><a href="#"><img src="dummy/slide-3.jpg" alt="Slide 3"></a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="col-md-3">
-								<div class="row">
-									<div class="col-sm-6 col-md-12">
-										<div class="latest-movie">
-											<a href="#"><img src="dummy/thumb-1.jpg" alt="Movie 1"></a>
-										</div>
-									</div>
-									<div class="col-sm-6 col-md-12">
-										<div class="latest-movie">
-											<a href="#"><img src="dummy/thumb-2.jpg" alt="Movie 2"></a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div> <!-- .row -->
-						<div class="row">
-							<div class="col-sm-6 col-md-3">
-								<div class="latest-movie">
-									<a href="#"><img src="dummy/thumb-3.jpg" alt="Movie 3"></a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="latest-movie">
-									<a href="#"><img src="dummy/thumb-4.jpg" alt="Movie 4"></a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="latest-movie">
-									<a href="#"><img src="dummy/thumb-5.jpg" alt="Movie 5"></a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="latest-movie">
-									<a href="#"><img src="dummy/thumb-6.jpg" alt="Movie 6"></a>
-								</div>
-							</div>
-						</div> <!-- .row -->
+<!-- Shell -->
+<div id="shell">
+	<!-- Header -->
+	<div id="header"><h1 id="logo"><a href="#"><img src="public/asset/image/the-elders-logo.png" alt=""></a></h1>
+
+			
+		
+		
+		
+		<div class="langue">
+			<span>Langue:</span>
+			<ul>
+			    <li><a class="fr" href="#">FR</a></li>
+			    <li><a class="en" href="#">EN</a></li>
+			    
+			</ul>
+		</div>
+		
+		<!-- Navigation -->
+		<div id="navigation">
+			<ul>
+			    <li><a class="active" href="#">HOME</a></li>
+			    <li><a href="#">NEWS</a></li>
+			    <li><a href="#">IN THEATERS</a></li>
+			    <li><a href="#">COMING SOON</a></li>
+			    <li><a href="#">CONTACT</a></li>
+			    <li><a href="#">ADVERTISE</a></li>
+			</ul>
+		</div>
+		<!-- end Navigation -->
+		
+		<!-- Sub-menu -->
+		<div id="sub-navigation"><div id="search">
+				<form action="home_submit" method="get" accept-charset="utf-8">
+									
+					<input type="text" name="search field" value="Recherche" id="search-field" title="Enter search here" class="blink search-field"/>
+					<input type="submit" value="GO!" class="search-button" />
+				</form>
+			</div>
+			
+			<ul>
+			    <li><a href="#">CLASSIQUE</a></li>
+			    <li><a href="#">D'HORREUR</a></li>
+			    <li><a href="#">POUR ENFANT</a></li>
+			    <li><a href="#">COMEDIE</a></li>
+			</ul>
+			
+		</div>
+		<!-- end Sub-Menu -->
+		
+	</div>
+	<!-- end Header -->
+	
+	<!-- Main -->
+	<div id="main">
+		<!-- Content -->
+		<div id="content">
+
+			<!-- Box -->
+			<div class="box">
+				
+				<!-- Movie -->
+				<div class="movie">
+					
+					<div class="movie-image">
+						<span class="play"><span class="name">titanic</span></span>
+						<a href="#"><img src="public/asset/image/movie1.jpg" alt="movie" /></a>
+					</div>
 						
-						<div class="row">
-							<div class="col-md-4">
-								<h2 class="section-title">December premiere</h2>
-								<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-								<ul class="movie-schedule">
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-								</ul> <!-- .movie-schedule -->
-							</div>
-							<div class="col-md-4">
-								<h2 class="section-title">November premiere</h2>
-								<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-								<ul class="movie-schedule">
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-								</ul> <!-- .movie-schedule -->
-							</div>
-							<div class="col-md-4">
-								<h2 class="section-title">October premiere</h2>
-								<p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-								<ul class="movie-schedule">
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-									<li>
-										<div class="date">16/12</div>
-										<h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-									</li>
-								</ul> <!-- .movie-schedule -->
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
 							</div>
 						</div>
+						<span class="comments">12</span>
 					</div>
-				</div> <!-- .container -->
-			</main>
+				</div>
+				<!-- end Movie -->
+				
+				<!-- Movie -->
+				<div class="movie">
+					<div class="movie-image">
+						<span class="play"><span class="name">wiplash</span></span>
+						<a href="#"><img src="public/asset/image/movie2.jpg" alt="movie" /></a>
+					</div>
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
+							</div>
+						</div>
+						<span class="comments">12</span>
+					</div>
+				</div>
+				<!-- end Movie -->
+				
+				<!-- Movie -->
+				<div class="movie">
+					<div class="movie-image">
+						<span class="play"><span class="name">SPIDER MAN 3</span></span>
+						<a href="#"><img src="public/asset/image/movie3.jpg" alt="movie" /></a>
+					</div>
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
+							</div>
+						</div>
+						<span class="comments">12</span>
+					</div>
+				</div>
+				<!-- end Movie -->
+				
+				<!-- Movie -->
+				<div class="movie">
+					<div class="movie-image">
+						<span class="play"><span class="name">VALKYRIE</span></span>
+						<a href="#"><img src="public/asset/image/movie4.jpg" alt="movie" /></a>
+					</div>
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
+							</div>
+						</div>
+						<span class="comments">12</span>
+					</div>
+				</div>
+				<!-- end Movie -->
+				
+				<!-- Movie -->
+				<div class="movie">
+					<div class="movie-image">
+						<span class="play"><span class="name">GLADIATOR</span></span>
+						<a href="#"><img src="public/asset/image/movie5.jpg" alt="movie" /></a>
+					</div>
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
+							</div>
+						</div>
+						<span class="comments">12</span>
+					</div>
+				</div>
+				<!-- end Movie -->
+				
+				<!-- Movie -->
+				
+			</div>
+			<!-- end Box -->
+			
+			<!-- Box -->
+			<div class="box">
+				
 
+				<!-- Movie -->
+				<div class="movie">
+					<div class="movie-image">
+						<span class="play"><span class="name">TRANSFORMERS</span></span>
+						<a href="#"><img src="public/asset/image/movie7.jpg" alt="movie" /></a>
+					</div>	
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
+							</div>
+						</div>
+						<span class="comments">12</span>
+					</div>
+				</div>
+				<!-- end Movie -->
+				
+				<!-- Movie -->
+				<div class="movie">
+					<div class="movie-image">
+						<span class="play"><span class="name">MAGNETO</span></span>
+						<a href="#"><img src="public/asset/image/movie8.jpg" alt="movie" /></a>
+					</div>
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
+							</div>
+						</div>
+						<span class="comments">12</span>
+					</div>
+				</div>
+				<!-- end Movie -->
+				
+				<!-- Movie -->
+				<div class="movie">
+					<div class="movie-image">
+						<span class="play"><span class="name">KUNG FU PANDA</span></span>
+						<a href="#"><img src="public/asset/image/movie9.jpg" alt="movie" /></a>
+					</div>	
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
+							</div>
+						</div>
+						<span class="comments">12</span>
+					</div>
+				</div>
+				<!-- end Movie -->
+				
+				<!-- Movie -->
+				<div class="movie">
+					<div class="movie-image">
+						<span class="play"><span class="name">EAGLE EYE</span></span>
+						<a href="#"><img src="public/asset/image/movie10.jpg" alt="movie" /></a>
+					</div>
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
+							</div>
+						</div>
+						<span class="comments">12</span>
+					</div>
+				</div>
+				<!-- end Movie -->
+				
+				<!-- Movie -->
+				<div class="movie">
+					<div class="movie-image">
+						<span class="play"><span class="name">NARNIA</span></span>
+						<a href="#"><img src="public/asset/image/movie11.jpg" alt="movie" /></a>
+					</div>
+					<div class="rating">
+						<p>RATING</p>
+						<div class="stars">
+							<div class="stars-in">
+								
+							</div>
+						</div>
+						<span class="comments">12</span>
+					</div>
+				</div>
+				<!-- end Movie -->
+				
+				
+				<!-- end Movie -->
+				<div class="cl">&nbsp;</div>
+			</div>
+			<!-- end Box -->
+			
+		
+			
+		</div>
+		<!-- end Content -->
 
+		
+		<div class="cl">&nbsp;</div>
+	</div>
+	<div id="news">
+			<div class="head">
+				<h3>NEWS</h3>
+				<p class="text-right"><a href="#">See all</a></p>
+			</div>
+			
+			<div class="content">
+				<p class="date">12.04.09</p>
+				<h4>Disney's A Christmas Carol</h4>
+				<p>&quot;Disney's A Christmas Carol,&quot; a multi-sensory thrill ride re-envisioned by Academy Award&reg;-winning filmmaker Robert Zemeckis, captures... </p>
+				<a href="#">Read more</a>
+			</div>
+			<div class="content">
+				<p class="date">11.04.09</p>
+				<h4>Where the Wild Things Are</h4>
+				<p>Innovative director Spike Jonze collaborates with celebrated author Maurice Sendak to bring one of the most beloved books of all time to the big screen in &quot;Where the Wild Things Are,&quot;...</p>
+				<a href="#">Read more</a>
+			</div>
+			<div class="content">
+				<p class="date">10.04.09</p>
+				<h4>The Box</h4>
+				<p>Norma and Arthur Lewis are a suburban couple with a young child who receive an anonymous gift bearing fatal and irrevocable consequences.</p>
+				<a href="#">Read more</a>
+			</div>
+		</div>
+		<!-- end NEWS -->
+		
+		<!-- Coming -->
+		<div id="coming">
+			<div class="head">
+				<h3>COMING SOON<strong>!</strong></h3>
+				<p class="text-right"><a href="#">See all</a></p>
+			</div>
+			<div class="content">
+				<h4>The Princess and the Frog </h4>
+					<a href="#"><img src="css/images/coming-soon1.jpg" alt="coming soon" /></a>
+				<p>Walt Disney Animation Studios presents the musical "The Princess and the Frog," an animated comedy set in the great city of New Orleans...</p>
+				<a href="#">Read more</a>
+			</div>
+			<div class="cl">&nbsp;</div>
+			<div class="content">
+				<h4>The Princess and the Frog </h4>
+					<a href="#"><img src="css/images/coming-soon2.jpg" alt="coming soon" /></a>
+				<p>Walt Disney Animation Studios presents the musical "The Princess and the Frog," an animated comedy set in the great city of New Orleans...</p>
+				<a href="#">Read more</a>
+			</div>
+			
+		</div>
+		<!-- end Coming -->
+		<div class="cl">&nbsp;</div>
 
+	<!-- end Main -->
 
+	<!-- Footer -->
+	<div id="footer">
+		<p>
+			<a href="#">HOME</a> <span>|</span>
+			<a href="#">NEWS</a> <span>|</span>
+			<a href="#">IN THEATERS</a> <span>|</span>
+			<a href="#">COMING SOON </a> <span>|</span>
+			<a href="#">LATERS TRAILERS</a> <span>|</span>
+			<a href="#">TOP RATED TRAILERS</a> <span>|</span>
+			<a href="#">MOST COMMENTED TRAILERS</a> <span>|</span>
+			<a href="#">ADVERTISE</a> <span>|</span>
+			<a href="#">CONTACT </a>
+		</p>
+		<p> &copy; 2009 Movie Hunter, LLC. All Rights Reserved.  Designed by <a href="http://chocotemplates.com" target="_blank" title="The Sweetest CSS Templates WorldWide">ChocoTemplates.com</a></p>
+	</div>
+	<!-- end Footer -->
+</div>
+<!-- end Shell -->
 </body>
 </html>
