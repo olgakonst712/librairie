dfc25cd50df616aa4be7a3affcb1abdc33f41600
olgakonst712 <olgakonstantinova712@gmail.com>
2017-10-24 15:37:39 +0200

    base de donner

---
+++ database/connect.php
@@ -0,0 +1,35 @@
+<?php
+
+function debug($var){
+	echo "<pre>";
+	print_r($var);
+	echo "</pre>";
+}
+
+$cfg_file = dirname( dirname(__FILE__) ).DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
+include $cfg_file;
+
+$dbconfig = $config['database'];
+
+try{
+
+// A changer : dbname=nomDeVotreBaseDeDonnées
+$bdd = new PDO("mysql:host=".$dbconfig['dbhost'].";dbname=".$dbconfig['dbname'].";charset=utf8", $dbconfig["dbuser"], $dbconfig["dbpass"]);
+}
+// On traite l'erreur (ex probleme de mot de passe, php rsique d'afficher
+// toute la ligne. Pour éviter ce problème : )
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+// Notre requete à la base de donnée
+// SELECT = prend moi; * = tout ;FROM = de quel tableau ? film = mettreLeNomDeVotreTable
+
+$query = $bdd->query('SELECT * FROM film' );
+
+
+$films = $query->fetchAll();
+//$imgGalerie = $bdd->query('SELECT * FROM photos LIMIT 50');
+
+
+?>
--- index.php
+++ index.php
@@ -1,293 +1,23 @@
-<?php 
+<?php include 'app/views/parts/header.php' ?>
 
 
-
-
- ?>
-
-<!DOCTYPE html>
-<html lang="fr"> 
-<head>
-	<meta charset="utf-8">
-	<meta content="musique" name="keywords">
-	<meta content="Download movies" name="description">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<div id="main">
 	
+	    
+		<?php foreach ($films as $film): ?>
 
-	<link rel="stylesheet" type="text/css" href="public/asset/css/style.css">
-	
-	<title>Elders</title>
-	
-	
-</head>
-
-<body>
-<!-- Shell -->
-<div id="shell">
-	<!-- Header -->
-	<div id="header"><h1 id="logo"><a href="#"><img src="public/asset/image/the-elders-logo.png" alt=""></a></h1>
-
-			
-		
-		
-		
-		<div class="langue">
-			<span>Langue:</span>
-			<ul>
-			    <li><a class="fr" href="#">FR</a></li>
-			    <li><a class="en" href="#">EN</a></li>
-			    
-			</ul>
-		</div>
-		
-		<!-- Navigation -->
-		<div id="navigation">
-			<ul>
-			    <li><a class="active" href="#">HOME</a></li>
-			    <li><a href="#">NEWS</a></li>
-			    <li><a href="#">IN THEATERS</a></li>
-			    <li><a href="#">COMING SOON</a></li>
-			    <li><a href="#">CONTACT</a></li>
-			    <li><a href="#">ADVERTISE</a></li>
-			</ul>
-		</div>
-		<!-- end Navigation -->
-		
-		<!-- Sub-menu -->
-		<div id="sub-navigation"><div id="search">
-				<form action="home_submit" method="get" accept-charset="utf-8">
-									
-					<input type="text" name="search field" value="Recherche" id="search-field" title="Enter search here" class="blink search-field"/>
-					<input type="submit" value="GO!" class="search-button" />
-				</form>
+			<div class="sectionfilm" >
+				
+		    	<img src="public/asset/image/<?php echo $film['film_photo_url']; ?>">
+		    	
 			</div>
-			
-			<ul>
-			    <li><a href="#">CLASSIQUE</a></li>
-			    <li><a href="#">D'HORREUR</a></li>
-			    <li><a href="#">POUR ENFANT</a></li>
-			    <li><a href="#">COMEDIE</a></li>
-			</ul>
-			
-		</div>
-		<!-- end Sub-Menu -->
-		
-	</div>
-	<!-- end Header -->
-	
-	<!-- Main -->
-	<div id="main">
-		<!-- Content -->
-		<div id="content">
 
-			<!-- Box -->
-			<div class="box">
-				
-				<!-- Movie -->
-				<div class="movie">
-					
-					<div class="movie-image">
-						<span class="play"><span class="name">titanic</span></span>
-						<a href="#"><img src="public/asset/image/movie1.jpg" alt="movie" /></a>
-					</div>
-						
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				<!-- Movie -->
-				<div class="movie">
-					<div class="movie-image">
-						<span class="play"><span class="name">wiplash</span></span>
-						<a href="#"><img src="public/asset/image/movie2.jpg" alt="movie" /></a>
-					</div>
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				<!-- Movie -->
-				<div class="movie">
-					<div class="movie-image">
-						<span class="play"><span class="name">SPIDER MAN 3</span></span>
-						<a href="#"><img src="public/asset/image/movie3.jpg" alt="movie" /></a>
-					</div>
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				<!-- Movie -->
-				<div class="movie">
-					<div class="movie-image">
-						<span class="play"><span class="name">VALKYRIE</span></span>
-						<a href="#"><img src="public/asset/image/movie4.jpg" alt="movie" /></a>
-					</div>
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				<!-- Movie -->
-				<div class="movie">
-					<div class="movie-image">
-						<span class="play"><span class="name">GLADIATOR</span></span>
-						<a href="#"><img src="public/asset/image/movie5.jpg" alt="movie" /></a>
-					</div>
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				<!-- Movie -->
-				
-			</div>
-			<!-- end Box -->
-			
-			<!-- Box -->
-			<div class="box">
-				
+        <?php endforeach ?>
 
-				<!-- Movie -->
-				<div class="movie">
-					<div class="movie-image">
-						<span class="play"><span class="name">TRANSFORMERS</span></span>
-						<a href="#"><img src="public/asset/image/movie7.jpg" alt="movie" /></a>
-					</div>	
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				<!-- Movie -->
-				<div class="movie">
-					<div class="movie-image">
-						<span class="play"><span class="name">MAGNETO</span></span>
-						<a href="#"><img src="public/asset/image/movie8.jpg" alt="movie" /></a>
-					</div>
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				<!-- Movie -->
-				<div class="movie">
-					<div class="movie-image">
-						<span class="play"><span class="name">KUNG FU PANDA</span></span>
-						<a href="#"><img src="public/asset/image/movie9.jpg" alt="movie" /></a>
-					</div>	
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				<!-- Movie -->
-				<div class="movie">
-					<div class="movie-image">
-						<span class="play"><span class="name">EAGLE EYE</span></span>
-						<a href="#"><img src="public/asset/image/movie10.jpg" alt="movie" /></a>
-					</div>
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				<!-- Movie -->
-				<div class="movie">
-					<div class="movie-image">
-						<span class="play"><span class="name">NARNIA</span></span>
-						<a href="#"><img src="public/asset/image/movie11.jpg" alt="movie" /></a>
-					</div>
-					<div class="rating">
-						<p>RATING</p>
-						<div class="stars">
-							<div class="stars-in">
-								
-							</div>
-						</div>
-						<span class="comments">12</span>
-					</div>
-				</div>
-				<!-- end Movie -->
-				
-				
-				<!-- end Movie -->
-				<div class="cl">&nbsp;</div>
-			</div>
-			<!-- end Box -->
-			
-		
-			
-		</div>
-		<!-- end Content -->
+    	
+ </div>	
+    <div class="cl">&nbsp;</div>
 
-		
-		<div class="cl">&nbsp;</div>
-	</div>
 	<div id="news">
 			<div class="head">
 				<h3>NEWS</h3>
@@ -313,7 +43,9 @@
 				<a href="#">Read more</a>
 			</div>
 		</div>
+
 		<!-- end NEWS -->
+		
 		
 		<!-- Coming -->
 		<div id="coming">
@@ -323,14 +55,14 @@
 			</div>
 			<div class="content">
 				<h4>The Princess and the Frog </h4>
-					<a href="#"><img src="css/images/coming-soon1.jpg" alt="coming soon" /></a>
+					<a href="#"><img src="public/asset/image/movie2.jpg" alt="coming soon" /></a>
 				<p>Walt Disney Animation Studios presents the musical "The Princess and the Frog," an animated comedy set in the great city of New Orleans...</p>
 				<a href="#">Read more</a>
 			</div>
 			<div class="cl">&nbsp;</div>
 			<div class="content">
 				<h4>The Princess and the Frog </h4>
-					<a href="#"><img src="css/images/coming-soon2.jpg" alt="coming soon" /></a>
+					<a href="#"><img src="public/asset/image/movie1.jpg" alt="coming soon" /></a>
 				<p>Walt Disney Animation Studios presents the musical "The Princess and the Frog," an animated comedy set in the great city of New Orleans...</p>
 				<a href="#">Read more</a>
 			</div>
@@ -339,25 +71,8 @@
 		<!-- end Coming -->
 		<div class="cl">&nbsp;</div>
 
-	<!-- end Main -->
 
+	<!-- end Main -->
+<?php include 'app/views/parts/footer.php' ?>
 	<!-- Footer -->
-	<div id="footer">
-		<p>
-			<a href="#">HOME</a> <span>|</span>
-			<a href="#">NEWS</a> <span>|</span>
-			<a href="#">IN THEATERS</a> <span>|</span>
-			<a href="#">COMING SOON </a> <span>|</span>
-			<a href="#">LATERS TRAILERS</a> <span>|</span>
-			<a href="#">TOP RATED TRAILERS</a> <span>|</span>
-			<a href="#">MOST COMMENTED TRAILERS</a> <span>|</span>
-			<a href="#">ADVERTISE</a> <span>|</span>
-			<a href="#">CONTACT </a>
-		</p>
-		<p> &copy; 2009 Movie Hunter, LLC. All Rights Reserved.  Designed by <a href="http://chocotemplates.com" target="_blank" title="The Sweetest CSS Templates WorldWide">ChocoTemplates.com</a></p>
-	</div>
-	<!-- end Footer -->
-</div>
-<!-- end Shell -->
-</body>
-</html>
+	
