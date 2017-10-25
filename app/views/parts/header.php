e53404abdb4d39cbe97437b5b5b3f8678268adc1
olgakonst712 <olgakonstantinova712@gmail.com>
2017-10-25 13:53:20 +0200

    style_single.php

--- app/views/parts/header.php
+++ app/views/parts/header.php
@@ -1,77 +1,59 @@
 <?php 
 include 'database/connect.php'
-
-
- ?>
- 
+?>
 
 <!DOCTYPE html>
-<html lang="fr"> 
+<html lang="fr">
 <head>
-	<meta charset="utf-8">
-	<meta content="musique" name="keywords">
-	<meta content="Download movies" name="description">
-	
-	
-
-	<link rel="stylesheet" type="text/css" href="public/asset/css/style.css">
-	
-	<title>Elders</title>
-	
-	
+<meta charset="utf-8">
+<meta content="musique" name="keywords">
+<meta content="Download movies" name="description">
+<link rel="stylesheet" type="text/css" href="public/asset/css/style.css">
+<title>Elders</title>
 </head>
 
 <body>
 <!-- Shell -->
 <div id="shell">
-	<!-- Header -->
-	<div id="header"><h1 id="logo"><a href="#"><img src="public/asset/image/the-elders-logo.png" alt=""></a></h1>
-
-			
-		
-		
-		
-		<div class="langue">
-			<span>Langue:</span>
-			<ul>
-			    <li><a class="fr" href="#">FR</a></li>
-			    <li><a class="en" href="#">EN</a></li>
-			    
-			</ul>
-		</div>
-		
-		<!-- Navigation -->
-		<div id="navigation">
-			<ul>
-			    <li><a class="active" href="#">HOME</a></li>
-			    <li><a href="#">NEWS</a></li>
-			    <li><a href="#">IN THEATERS</a></li>
-			    <li><a href="#">COMING SOON</a></li>
-			    <li><a href="#">CONTACT</a></li>
-			    <li><a href="#">ADVERTISE</a></li>
-			</ul>
-		</div>
-		<!-- end Navigation -->
-		
-		<!-- Sub-menu -->
-		<div id="sub-navigation"><div id="search">
-				<form action="home_submit" method="get" accept-charset="utf-8">
-									
-					<input type="text" name="search field" value="Recherche" id="search-field" title="Enter search here" class="blink search-field"/>
-					<input type="submit" value="GO!" class="search-button" />
-				</form>
-			</div>
-			
-			<ul>
-			    <li><a href="#">CLASSIQUE</a></li>
-			    <li><a href="#">D'HORREUR</a></li>
-			    <li><a href="#">POUR ENFANT</a></li>
-			    <li><a href="#">COMEDIE</a></li>
-			</ul>
-			
-		</div>
-		<!-- end Sub-Menu -->
-		
-	</div>
-	<!-- end Header -->
-	
+<!-- Header -->
+<div id="header">
+  <h1 id="logo"><a href="#"><img src="public/asset/image/the-elders-logo.png" alt=""></a></h1>
+  <div class="langue"> <span>Langue:</span>
+    <ul>
+      <li><a class="fr" href="#">FR</a></li>
+      <li><a class="en" href="#">EN</a></li>
+    </ul>
+  </div>
+  
+  <!-- Navigation -->
+  <div id="navigation">
+    <ul>
+      <li><a class="active" href="#">HOME</a></li>
+      <li><a href="#">NEWS</a></li>
+      <li><a href="#">IN THEATERS</a></li>
+      <li><a href="#">COMING SOON</a></li>
+      <li><a href="#">CONTACT</a></li>
+      <li><a href="#">ADVERTISE</a></li>
+    </ul>
+  </div>
+  <!-- end Navigation --> 
+  
+  <!-- Sub-menu -->
+  <div id="sub-navigation">
+    <div id="search">
+      <form action="home_submit" method="get" accept-charset="utf-8">
+        <input type="text" name="search field" value="Recherche" id="search-field" title="Enter search here" class="blink search-field"/>
+        <input type="submit" value="GO!" class="search-button" />
+      </form>
+    </div>
+    <ul>
+      <li><a href="#">CLASSIQUE</a></li>
+      <li><a href="#">D'HORREUR</a></li>
+      <li><a href="#">POUR ENFANT</a></li>
+      <li><a href="#">COMEDIE</a></li>
+    </ul>
+  </div>
+  <!-- end Sub-Menu --> 
+  
+</div>
+<!-- end Header --> 
--- index.php
+++ index.php
@@ -1,78 +1,55 @@
 <?php include 'app/views/parts/header.php' ?>
-
-
-<div id="main">
-	
-	    
-		<?php foreach ($films as $film): ?>
-
-			<div class="sectionfilm"><a href="app/views/parts/single.php"><img src="public/asset/image/<?php echo $film['film_photo_url']; ?>"></a>
-				
-		    	
-		    	
-			</div>
-
-        <?php endforeach ?>
-
-    	
- </div>	
+  <div id="main">
+    <?php foreach ($films as $film): ?>
+    <div class="sectionfilm"><a href="app/views/parts/single.php"><img src="public/asset/image/<?php echo $film['film_photo_url']; ?>"></a> </div>
+    <?php endforeach ?>
+  </div>
+  <div class="cl">&nbsp;</div>
+  <div id="news">
+    <div class="head">
+      <h3>NEWS</h3>
+      <p class="text-right"><a href="#">See all</a></p>
+    </div>
+    <div class="content">
+      <p class="date">12.04.09</p>
+      <h4>Disney's A Christmas Carol</h4>
+      <p>&quot;Disney's A Christmas Carol,&quot; a multi-sensory thrill ride re-envisioned by Academy Award&reg;-winning filmmaker Robert Zemeckis, captures... </p>
+      <a href="#">Read more</a> </div>
+    <div class="content">
+      <p class="date">11.04.09</p>
+      <h4>Where the Wild Things Are</h4>
+      <p>Innovative director Spike Jonze collaborates with celebrated author Maurice Sendak to bring one of the most beloved books of all time to the big screen in &quot;Where the Wild Things Are,&quot;...</p>
+      <a href="#">Read more</a> </div>
+    <div class="content">
+      <p class="date">10.04.09</p>
+      <h4>The Box</h4>
+      <p>Norma and Arthur Lewis are a suburban couple with a young child who receive an anonymous gift bearing fatal and irrevocable consequences.</p>
+      <a href="#">Read more</a> </div>
+  </div>
+  
+  <!-- end NEWS --> 
+  
+  <!-- Coming -->
+  <div id="coming">
+    <div class="head">
+      <h3>COMING SOON<strong>!</strong></h3>
+      <p class="text-right"><a href="#">See all</a></p>
+    </div>
+    <div class="content">
+      <h4>The Princess and the Frog </h4>
+      <a href="#"><img src="public/asset/image/movie2.jpg" alt="coming soon" /></a>
+      <p>Walt Disney Animation Studios presents the musical "The Princess and the Frog," an animated comedy set in the great city of New Orleans...</p>
+      <a href="#">Read more</a> </div>
     <div class="cl">&nbsp;</div>
-
-	<div id="news">
-			<div class="head">
-				<h3>NEWS</h3>
-				<p class="text-right"><a href="#">See all</a></p>
-			</div>
-			
-			<div class="content">
-				<p class="date">12.04.09</p>
-				<h4>Disney's A Christmas Carol</h4>
-				<p>&quot;Disney's A Christmas Carol,&quot; a multi-sensory thrill ride re-envisioned by Academy Award&reg;-winning filmmaker Robert Zemeckis, captures... </p>
-				<a href="#">Read more</a>
-			</div>
-			<div class="content">
-				<p class="date">11.04.09</p>
-				<h4>Where the Wild Things Are</h4>
-				<p>Innovative director Spike Jonze collaborates with celebrated author Maurice Sendak to bring one of the most beloved books of all time to the big screen in &quot;Where the Wild Things Are,&quot;...</p>
-				<a href="#">Read more</a>
-			</div>
-			<div class="content">
-				<p class="date">10.04.09</p>
-				<h4>The Box</h4>
-				<p>Norma and Arthur Lewis are a suburban couple with a young child who receive an anonymous gift bearing fatal and irrevocable consequences.</p>
-				<a href="#">Read more</a>
-			</div>
-		</div>
-
-		<!-- end NEWS -->
-		
-		
-		<!-- Coming -->
-		<div id="coming">
-			<div class="head">
-				<h3>COMING SOON<strong>!</strong></h3>
-				<p class="text-right"><a href="#">See all</a></p>
-			</div>
-			<div class="content">
-				<h4>The Princess and the Frog </h4>
-					<a href="#"><img src="public/asset/image/movie2.jpg" alt="coming soon" /></a>
-				<p>Walt Disney Animation Studios presents the musical "The Princess and the Frog," an animated comedy set in the great city of New Orleans...</p>
-				<a href="#">Read more</a>
-			</div>
-			<div class="cl">&nbsp;</div>
-			<div class="content">
-				<h4>The Princess and the Frog </h4>
-					<a href="#"><img src="public/asset/image/movie1.jpg" alt="coming soon" /></a>
-				<p>Walt Disney Animation Studios presents the musical "The Princess and the Frog," an animated comedy set in the great city of New Orleans...</p>
-				<a href="#">Read more</a>
-			</div>
-			
-		</div>
-		<!-- end Coming -->
-		<div class="cl">&nbsp;</div>
-
-
-	<!-- end Main -->
-<?php include 'app/views/parts/footer.php' ?>
-	<!-- Footer -->
-	
+    <div class="content">
+      <h4>The Princess and the Frog </h4>
+      <a href="#"><img src="public/asset/image/movie1.jpg" alt="coming soon" /></a>
+      <p>Walt Disney Animation Studios presents the musical "The Princess and the Frog," an animated comedy set in the great city of New Orleans...</p>
+      <a href="#">Read more</a> </div>
+  </div>
+  <!-- end Coming -->
+  <div class="cl">&nbsp;</div>
+  
+  <!-- end Main -->
+  <?php include 'app/views/parts/footer.php' ?>
+<!-- Footer -->
